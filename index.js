c7cab132a88b42b4531c9922c99c74e03c0806ea
pooja-zagade <poojazagade2001@gmail.com>
2025-09-22 16:45:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello from dummy Node.js app");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from dummy Node.js app');
+});
+
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
+
