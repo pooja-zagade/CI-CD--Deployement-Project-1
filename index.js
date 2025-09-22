d849964d9552f14497080b19a5ef26ab45f0562d
pooja-zagade <poojazagade2001@gmail.com>
2025-09-22 17:09:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end('Hello from dummy Node.js app');
-});
-
-server.listen(3000, '0.0.0.0', () => {
-  console.log('Server running on port 3000');
-});
-
+console.log("Hello from dummy Node.js app");
